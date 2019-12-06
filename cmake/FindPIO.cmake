69d8b44be634e269f173f69f6f75e3f84a49faf8
Michael Duda <duda@ucar.edu>
2019-12-06 14:27:33 -0700

    Decide whether to define USE_PIO2 based on new PIO_VERSION global property
    The find_package script for PIO now defined a global attribute, PIO_VERSION,
    as either "1" or "2", depending on which version of the PIO library the
    script found. This property is used to define the compilation macro USE_PIO2.

--- cmake/FindPIO.cmake
+++ cmake/FindPIO.cmake
@@ -3,6 +3,7 @@
 #  PIO_FOUND - System has netCDF
 #  PIO_INCLUDE_DIRS - The netCDF include directories
 #  PIO_LIBRARIES - The libraries needed to use netCDF
+#  PIO_VERSION - A global property indicating the PIO API: either 1 or 2
 
 find_path(PIO_INCLUDE_DIR pio.mod PIO.mod HINTS ENV PIO ENV PIO_PATH PATH_SUFFIXES include)
 find_library(PIO1_C_LIBRARY libpio.a HINTS ENV PIO ENV PIO_PATH PATH_SUFFIXES lib)
@@ -11,8 +12,10 @@ find_library(PIO_F_LIBRARY libpiof.a HINTS ENV PIO ENV PIO_PATH PATH_SUFFIXES li
 
 if(PIO1_C_LIBRARY)
    set(PIO_C_LIBRARY ${PIO1_C_LIBRARY})
+   set_property(GLOBAL PROPERTY PIO_VERSION 1)
 elseif(PIO2_C_LIBRARY)
    set(PIO_C_LIBRARY ${PIO2_C_LIBRARY})
+   set_property(GLOBAL PROPERTY PIO_VERSION 2)
 endif()
 
 set(PIO_INCLUDE_DIRS ${PIO_INCLUDE_DIR})
